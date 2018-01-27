6b660f5cc4de76e422366494d52146a8b45da6fc
Hannah Redler <hannahjredler@gmail.com>
2018-01-27 15:57:04 +0100

    Fixed login - wasn't redirecting on initial login

--- go-for-eat/components/Buttons/facebookButton.js
+++ go-for-eat/components/Buttons/facebookButton.js
@@ -3,32 +3,15 @@ import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import styles from './styles';
 import { connect } from 'react-redux';
-import { loginUser } from '../../actions';
+// import { loginUser } from '../../actions';
+import PropTypes from 'prop-types';
+
 
 class FacebookButton extends Component {
 
-  loginFacebook = async () => {
-    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('442248476177832', {
-      permissions: ['public_profile', 'email', 'user_birthday'],
-    });
-    if (type === 'success') {
-      return fetch(`https://graph.facebook.com/me?access_token=${token}`)
-      .then(res => res.json())
-      .then(data => {
-        data.accessToken = token;
-        data.network = 'facebook';
-        const allowed = ['id', 'accessToken', 'network'];
-        const filtered = Object.keys(data)
-          .filter(key => allowed.includes(key))
-          .reduce((obj, key) => {
-            obj[key] = data[key];
-            return obj;
-          }, {});
-        this.props.serverAuth(filtered);
-      })
-      .catch(err => console.log(err));
-    }
-  }
+  static propTypes = {
+    navigation: PropTypes.object
+  };
 
   render() {
     return (
@@ -40,7 +23,7 @@ class FacebookButton extends Component {
           }}
           title='Login with Facebook'
           buttonStyle={styles.login_button__fb}
-          onPress={this.loginFacebook}
+          onPress={this.props.loginFacebook}
         />
       </View>
     );
