97e55584c89c6208b3da5d3ec62e2516a57d6110
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2018-01-27 15:01:13 +0100

    created event component and styled

--- go-for-eat/screens/Home/Home.js
+++ go-for-eat/screens/Home/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView, Image, Text } from 'react-native';
 import s from './styles';
+import { Event } from '../../components/Event';
+import _ from 'lodash';
+
 
 class Home extends Component {
-
+  constructor(props){
+    super(props);
+  }
   render() {
     return  (
       <View style={s.container}>
@@ -14,82 +19,9 @@ class Home extends Component {
           <View style={s.list_dragBar}>
             <View style={s.list_dragBar_line}></View>
           </View>
-          <View style={s.event}>
-            <View style={s.event_detail}>
-              <Text style={s.event_detail_eventName}> Nome, Location Restaurant </Text>
-              <Text style={s.event_detail_address}> Carrer Sant Martì 13, Barcelona </Text>
-              <View style={s.event_detail_time}>
-                <Text style={s.event_detail_time_text}> 23:45 </Text>
-              </View>
-            </View>
-            <View style={s.event_distance}>
-              <Text style={s.event_distance_number}> 203 </Text>
-              <Text style={s.event_distance_text}> m </Text>
-            </View>
-            <View style={s.event_spots}>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_free}></View>
-              <View style={s.event_spots_free}></View>
-            </View>
-          </View>
-          <View style={s.event}>
-            <View style={s.event_detail}>
-              <Text style={s.event_detail_eventName}> Nome, Location Restaurant </Text>
-              <Text style={s.event_detail_address}> Carrer Sant Martì 13, Barcelona </Text>
-              <View style={s.event_detail_time}>
-                <Text style={s.event_detail_time_text}> 23:45 </Text>
-              </View>
-            </View>
-            <View style={s.event_distance}>
-              <Text style={s.event_distance_number}> 203 </Text>
-              <Text style={s.event_distance_text}> m </Text>
-            </View>
-            <View style={s.event_spots}>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_free}></View>
-              <View style={s.event_spots_free}></View>
-            </View>
-          </View>
-          <View style={s.event}>
-            <View style={s.event_detail}>
-              <Text style={s.event_detail_eventName}> Nome, Location Restaurant </Text>
-              <Text style={s.event_detail_address}> Carrer Sant Martì 13, Barcelona </Text>
-              <View style={s.event_detail_time}>
-                <Text style={s.event_detail_time_text}> 23:45 </Text>
-              </View>
-            </View>
-            <View style={s.event_distance}>
-              <Text style={s.event_distance_number}> 203 </Text>
-              <Text style={s.event_distance_text}> m </Text>
-            </View>
-            <View style={s.event_spots}>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_free}></View>
-              <View style={s.event_spots_free}></View>
-            </View>
-          </View>
-          <View style={s.event}>
-            <View style={s.event_detail}>
-              <Text style={s.event_detail_eventName}> Nome, Location Restaurant </Text>
-              <Text style={s.event_detail_address}> Carrer Sant Martì 13, Barcelona </Text>
-              <View style={s.event_detail_time}>
-                <Text style={s.event_detail_time_text}> 23:45 </Text>
-              </View>
-            </View>
-            <View style={s.event_distance}>
-              <Text style={s.event_distance_number}> 203 </Text>
-              <Text style={s.event_distance_text}> m </Text>
-            </View>
-            <View style={s.event_spots}>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_full}></View>
-              <View style={s.event_spots_free}></View>
-              <View style={s.event_spots_free}></View>
-            </View>
-          </View>
+          {  _.range(5).map((el) => {
+            return  <Event key={el} />;
+          })}
         </ScrollView>
       </View>
     );
