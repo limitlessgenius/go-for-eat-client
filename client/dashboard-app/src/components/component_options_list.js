b5e2cc30f5709945b4b7e4cb2188b0c17e52fce7
José Manuel <expressmillionaire@hotmail.com>
2018-04-28 18:15:34 +0200

    component based custom styling

--- client/dashboard-app/src/components/component_options_list.js
+++ client/dashboard-app/src/components/component_options_list.js
@@ -3,17 +3,20 @@
 
 import React, { Component } from 'react'
 
+import { Tabs, Tab } from 'react-materialize'
+
 class OptionsList extends Component {
 	render() {
 		return (
 			<div className="options-dashboard">
-				<ul className="options-list tabs">
-					<li className="tab option-detail">ALL</li>
-					<li className="tab option-detail">PAST</li>
-					<li className="tab option-detail">TODAY</li>
-					<li className="tab option-detail">FUTURE</li>
-				</ul>
+				<Tabs className="options-list tabs">
+					<Tab title="ALL" />
+					<Tab title="PAST" />
+					<Tab title="TODAY" />
+					<Tab title="FUTURE" />
+				</Tabs>
 			</div>
+
 		)
 	}
 }
